03940ebb0b7b19460932e69987d8c36ac5779b5a
Kalam :p <63474858+YourKalamity@users.noreply.github.com>
2020-07-24 15:12:13 +0100

    Functions - Start!

---
+++ functions/target/rls/debug/deps/functions-510933c219963c3f.d
@@ -0,0 +1,5 @@
+c:\Users\kalam\Documents\GitHub\rust-projects\functions\target\rls\debug\deps\functions-510933c219963c3f.rmeta: src\main.rs
+
+c:\Users\kalam\Documents\GitHub\rust-projects\functions\target\rls\debug\deps\functions-510933c219963c3f.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/rls/debug/deps/functions-8d08fb4ad7a3f2bd.d
@@ -0,0 +1,5 @@
+c:\Users\kalam\Documents\GitHub\rust-projects\functions\target\rls\debug\deps\functions-8d08fb4ad7a3f2bd.rmeta: src\main.rs
+
+c:\Users\kalam\Documents\GitHub\rust-projects\functions\target\rls\debug\deps\functions-8d08fb4ad7a3f2bd.d: src\main.rs
+
+src\main.rs:
