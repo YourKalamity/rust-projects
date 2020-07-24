83c296dc2f8c4f5a3f3cfb88bc0324f38b05ce41
Kalam :p <63474858+YourKalamity@users.noreply.github.com>
2020-07-24 15:05:06 +0100

    Variables - Finished!

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     println!("The value of X is {}", x);
     x = 6;
     println!("The new value of X is {}", x);
+    const MAX_POINTS: u32 = 100_000;
+    println!("The maximum ammount of points is {}", MAX_POINTS);
+    let spaces = "    ";
+    let spaces = spaces.len();
 }
