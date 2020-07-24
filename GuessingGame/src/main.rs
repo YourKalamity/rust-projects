fe54c70785aac76e89a6c3cbd521de585f825828
Kalam :p <63474858+YourKalamity@users.noreply.github.com>
2020-07-24 14:45:18 +0100

    Guessing Game finished!

--- GuessingGame/src/main.rs
+++ GuessingGame/src/main.rs
@@ -4,22 +4,38 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("The secret number is {}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed : {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("That's correct!"),
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("{} is not a valid number", guess);
+                    continue
+                }
+            };
+
+    
+        println!("You guessed : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("That's correct");
+                break;
+            }
+        }
+    
     }
 }
