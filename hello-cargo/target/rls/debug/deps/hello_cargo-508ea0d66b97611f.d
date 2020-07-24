8f192f472fd56fea10df9266ada27cfeb7a89b44
Kalam :p <63474858+YourKalamity@users.noreply.github.com>
2020-07-24 12:25:04 +0100

    Hello Cargo

---
+++ hello-cargo/target/rls/debug/deps/hello_cargo-508ea0d66b97611f.d
@@ -0,0 +1,5 @@
+c:\Users\kalam\Documents\GitHub\rust-projects\hello-cargo\target\rls\debug\deps\hello_cargo-508ea0d66b97611f.rmeta: src\main.rs
+
+c:\Users\kalam\Documents\GitHub\rust-projects\hello-cargo\target\rls\debug\deps\hello_cargo-508ea0d66b97611f.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/rls/debug/deps/hello_cargo-58e15787718a9311.d
@@ -0,0 +1,5 @@
+c:\Users\kalam\Documents\GitHub\rust-projects\hello-cargo\target\rls\debug\deps\hello_cargo-58e15787718a9311.rmeta: src\main.rs
+
+c:\Users\kalam\Documents\GitHub\rust-projects\hello-cargo\target\rls\debug\deps\hello_cargo-58e15787718a9311.d: src\main.rs
+
+src\main.rs:
